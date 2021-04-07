8b4dfb46bd5ff7f55e9c903f965e1438cb020151
Nicolás Mahnic <nmahnic@pdssa.com>
2021-04-06 21:49:55 -0300

    first commit

---
+++ app/src/main/java/com/nicomahnic/dadm/clase1/MainActivity.kt
@@ -0,0 +1,43 @@
+package com.nicomahnic.dadm.clase1
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.util.Log
+import android.widget.Button
+import android.widget.TextView
+
+class MainActivity : AppCompatActivity() {
+
+    lateinit var tvMain : TextView
+    lateinit var btnUp: Button
+    lateinit var btnDown: Button
+    lateinit var btnChange: Button
+
+    var tvMainSize: Float = 20.0F
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        tvMain = findViewById(R.id.tvMain)
+        btnChange = findViewById(R.id.btnChange)
+        btnUp = findViewById(R.id.btnUp)
+        btnDown = findViewById(R.id.btnDown)
+
+        btnChange.setOnClickListener {
+            tvMain.text = "CHAU"
+        }
+
+        btnUp.setOnClickListener {
+            tvMainSize = tvMainSize + 1.0F
+            Log.d("NM", "UP: ${tvMainSize}")
+            tvMain.textSize = tvMainSize
+        }
+
+        btnDown.setOnClickListener {
+            tvMainSize = tvMainSize - 1.0F
+            Log.d("NM", "DOWN: ${tvMainSize}")
+            tvMain.textSize = tvMainSize
+        }
+    }
+}
